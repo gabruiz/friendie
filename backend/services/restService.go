553d5f0e2bb00e8135c464bdcd61caed0ee794f9
gabruiz <gabruiz91@gmail.com>
2022-07-18 19:10:49 +0200

    fix endpoints

--- backend/services/restService.go
+++ backend/services/restService.go
@@ -1,7 +1,7 @@
 package services
 
 import (
-	"backend/services/endpoints"
+	"backend/endpoints"
 	"log"
 	"net/http"
 
@@ -12,11 +12,8 @@ func StartRestService() {
 	log.Println("Loading Rest Service")
 
 	router := mux.NewRouter()
-	router = endpoints.InitAccountRestService(router)
-	router = endpoints.InitMainRestService(router)
-
-	// router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { mainHandleRequests(w, r, router) }).Methods(http.MethodGet)
-	// router.HandleFunc("/accounts", func(w http.ResponseWriter, r *http.Request) { accountHandleRequests(w, r, router) }).Methods(http.MethodPost)
+	endpoints.InitAccountRestService(router)
+	endpoints.InitMainRestService(router)
 
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(":8083", router))
